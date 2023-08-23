bde5cac09aea7bfd38cf6b6f0724a9f388ccd68f
Sardorbek <sardorbekcoderjon@gmail.com>
2023-08-23 13:02:19 +0500

    Hammasi joyida

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from '../Home/Home';
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
