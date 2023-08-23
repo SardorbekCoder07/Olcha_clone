0f2d9d02cd864e64972b103c3a846700db06875e
Sardorbek <sardorbekcoderjon@gmail.com>
2023-08-24 00:34:06 +0500

    Navbar deyarl taxt footer esa tayyor

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -2,11 +2,17 @@ import logotip from './image/navbaTop/logotip.svg'
 import navbarTop from './image/navbaTop/navbarTop.png'
 import search from './image/navbaTop/search.svg'
 import statistic from './image/navbaTop/statistic.svg'
+import person from './image/navbaTop/person.svg'
+import shop from './image/navbaTop/shop.svg'
+import heart from './image/navbaTop/heart.svg'
 
 
 export {
   logotip,
   navbarTop,
   search,
-  statistic
+  statistic,
+  person,
+  shop,
+  heart
 }
--- src/components/navbar comps/search nav comp/SearchNav.js
+++ src/components/navbar comps/search nav comp/SearchNav.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
 import './searchNav.css'
 import { Link } from 'react-router-dom'
-import { logotip, search, statistic } from '../../assets'
+import { heart, logotip, person, search, shop, statistic } from '../../assets'
+import { Button, Input, InputGroup } from 'reactstrap'
 
 
 
@@ -9,15 +10,15 @@ const SearchNav = () => {
   useEffect(() => {
     const navIcons = document.querySelectorAll('#nav-icon3');
     const navIcons2 = document.querySelectorAll('#btn-click');
-      navIcons2[0].addEventListener('click', () => {
-        navIcons[0].classList.toggle('open');
+    navIcons2[0].addEventListener('click', () => {
+      navIcons[0].classList.toggle('open');
     });
   }, [])
   return (
     <div className='search_bottom'>
       <div className='bottom_search'>
         <Link to='/'>
-          <img src={logotip} alt='logo' />
+          <img src={logotip} alt='logo' className='images' />
         </Link>
         <button className='burger ' id='btn-click'>
           <div id="nav-icon3" className='float-start '>
@@ -29,15 +30,34 @@ const SearchNav = () => {
           <span className='float-end spans mt- mx-3'> Katalog</span>
         </button>
         <div className='bottom-search_header'>
-          <div className='search-wrapper'></div>
-          <div className='form-search'>
-            <input type="text" className='form-search_input' placeholder="Katalog bo'yicha qidirish" />
-            <img className='search_image' src={search} alt=" input search" />
+          <div className='navbar-search'>
+            <InputGroup>
+              <Input type="text" className='navbar-search_input' placeholder="Katalog bo'yicha qidirish"  />
+              <Button color='light' className='p-0 ' >
+                <img className='search_image' src={search} alt=" input search" />
+              </Button>
+            </InputGroup>
+           
+            
           </div>
         </div>
-        <div className='search-header_actions'>
-          <img src={statistic} alt="rasm" />
-        </div>
+        <figure className="figure">
+          <img src={statistic} alt="rasm" className='figure-img img-fluid  ' />
+          <figcaption className="text-center figure-caption">Taqqoslash</figcaption>
+        </figure>
+        <figure className="figure">
+          <img src={heart} alt="rasm" className='figure-img img-fluid align-items-center  ms-3' />
+          <figcaption className="text-center figure-caption">Taqqoslash</figcaption>
+        </figure>
+        <figure className="figure">
+          <img src={shop} alt="rasm" className='figure-img img-fluid ' />
+          <figcaption className="text-center figure-caption">Taqqoslash</figcaption>
+        </figure>
+        <figure className="figure">
+          <img src={person} alt="rasm" className='figure-img img-fluid  ' />
+          <figcaption className="text-center figure-caption">Taqqoslash</figcaption>
+        </figure>
+
       </div>
     </div>
   )
